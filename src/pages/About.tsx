ff623f14a75ffe53da50106b416f96fa49bd3b9f
meganemcclure <meganemcclure@gmail.com>
2024-04-27 22:06:19 -0400

    update about me page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,12 +2,12 @@ import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
 
-import wipImg from './../images/work-in-progress.png';
+import heroImg from './../images/IMG_0099 (5).jpg';
 
 export default function About() {
     return <>
       <Nav />
-      <Hero img={wipImg} header='WIP' body='This page is a work in progress... please check back later.' />
+      <Hero img={heroImg} header='About Me' body='Hi there! I’m Megan, a full-stack developer with a strong affinity for frontend development and a budding passion for UI/UX design. When I’m not coding or designing, you’ll find me spinning my own yarn and knitting.' />
       <Footer />
     </>
 }
