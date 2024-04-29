70ea6e63f617c5575a0d275a04c1a366adc3bc97
meganemcclure <meganemcclure@gmail.com>
2024-04-29 11:42:53 -0400

    cleanup

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -1,45 +1,19 @@
-import React, { useState, useEffect, useRef, FormEvent } from 'react';
+import { useRef, FormEvent } from 'react';
 import emailjs from '@emailjs/browser';
-import axios from 'axios';
-
-interface EmailFormData {
-  user_name: string;
-  user_email: string;
-  message: string;
-}
+// import 'dotenv/config';
 
 function EmailForm () {
   const form = useRef<HTMLFormElement>(null);
-//   const [publicKey, setPublicKey] = useState('');
-
-//   useEffect(() => {
-//     const fetchPublicKey = async () => {
-//       try {
-//         const response = await axios.get('/api/github-secret');
-//         setPublicKey(response.data.PUBLIC_EMAIL_KEY);
-//         console.log(`Fetched public key: ${publicKey}`)
-//       } catch (error) {
-//         console.error('Error fetching GitHub secret:', error);
-//       }
-//     };
-
-//     fetchPublicKey();
-//   }, []);
 
   const sendEmail = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (form.current) {
-      const formData = new FormData(form.current);
-      const emailData: EmailFormData = {
-        user_name: formData.get('user_name') as string,
-        user_email: formData.get('user_email') as string,
-        message: formData.get('message') as string,
-      };
+    console.log(`process.env: ${JSON.stringify(process.env)}`)
 
+    if (form.current) {
       emailjs
-        .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
-          publicKey: 'YOUR_PUBLIC_KEY',
+        .sendForm(process.env.SERVICE_ID || '', process.env.TEMPLATE_ID || '', form.current, {
+          publicKey: process.env.EMAIL_KEY || '',
         })
         .then(
           () => {
