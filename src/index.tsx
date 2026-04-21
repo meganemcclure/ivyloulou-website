01950b24cc91ec8d9ca49ecc6b459b8421109173
Megan McClure <meganemcclure@gmail.com>
2026-04-21 19:48:18 -0400

    start homepage redesign

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css';
+import "./index.css";
 
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import { About, Home, Work, BuddySystemCaseStudy, CanadaPostCaseStudy, RFECYCaseStudy, Error } from './pages';
+import forestImage from "./images/alexandra-6HE9J1q1FUk-unsplash.jpg";
+import lace from "./images/Transparent_Lace_PNG_Clip_Art_Image.png";
+import {
+	faInstagram,
+	faGithub,
+	faLinkedin,
+	faGit,
+} from "@fortawesome/free-brands-svg-icons";
+
+import { HashRouter, Routes, Route } from "react-router-dom";
+import {
+	About,
+	Home,
+	Work,
+	BuddySystemCaseStudy,
+	CanadaPostCaseStudy,
+	RFECYCaseStudy,
+	Error,
+} from "./pages";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById("root") as HTMLElement,
 );
+
 root.render(
-  <React.StrictMode>
-      <HashRouter>
+	<React.StrictMode>
+		{/* <HashRouter>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/home' element={<Home />} />
@@ -22,6 +41,40 @@ root.render(
           <Route path='/about' element={<About />} />
           <Route path='*' element={<Error />} />
         </Routes>
-      </HashRouter>
-  </React.StrictMode>
+      </HashRouter> */}
+		<div className="hero-container">
+			<img src={forestImage} className="hero-image" />
+			{/* <img src={lace} className="lace-overlay" /> */}
+		</div>
+		<div className="nav-container">
+			<div className="nav-icons">
+				<a href="" className="homepage-text">
+					<FontAwesomeIcon icon={faInstagram} />
+				</a>
+				<a href="" className="homepage-text">
+					<FontAwesomeIcon icon={faGithub} />
+				</a>
+				<a href="" className="homepage-text">
+					<FontAwesomeIcon icon={faLinkedin} />
+				</a>
+			</div>
+			<p className="homepage-text barcode-text">Megan McClure</p>
+		</div>
+		<div className="hero-container homepage-text">
+			<h1 className="hero-text">Welcome</h1>
+			<div className="homepage-nav-buttons">
+				<a href="/resume" className="homepage-text">
+					resume
+				</a>
+				<p>●</p>
+				<a href="/knitting" className="homepage-text">
+					knitting
+				</a>
+				<p>●</p>
+				<a href="about" className="homepage-text">
+					about
+				</a>
+			</div>
+		</div>
+	</React.StrictMode>,
 );
